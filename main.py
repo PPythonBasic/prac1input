baa605f59ae7e872e975c01b338e064a9f1f0896
JamMerRy <leejungkok556677@gmail.com>
2024-01-04 21:15:21 +0700

    feat: completed

--- main.py
+++ main.py
@@ -8,9 +8,21 @@
 """
 
 ## input width
-
+width = int(input("Enter your width : "))
 
 ## input height
+height = int(input("Enter your width : "))
+
+result = width + height
 
 
 ## print
+print(
+    "ผลลัพธ์ของ width ",
+    width,
+    "ซม และ ผลลัพธ์ของ height ",
+    height,
+    "ซม คือ sum",
+    result,
+    "ซม",
+)
